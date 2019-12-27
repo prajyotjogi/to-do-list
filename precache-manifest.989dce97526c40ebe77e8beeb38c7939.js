c488ffaf3abf42038e90eedaf812848b2626ac48
prajyotjogi <32704016+prajyotjogi@users.noreply.github.com>
2019-12-27 22:06:09 +0530

    Updates

---
+++ precache-manifest.989dce97526c40ebe77e8beeb38c7939.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45bff56d05aafc590866241a10ecbea9",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "dd5186889e9a1f29f8d6",
+    "url": "/to-do-list/static/css/main.f46759b3.chunk.css"
+  },
+  {
+    "revision": "46a95bd40a65af8e9c1d",
+    "url": "/to-do-list/static/js/2.f87e676c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/to-do-list/static/js/2.f87e676c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd5186889e9a1f29f8d6",
+    "url": "/to-do-list/static/js/main.9a0c12b8.chunk.js"
+  },
+  {
+    "revision": "41eaec7e616a8df64ff6",
+    "url": "/to-do-list/static/js/runtime-main.8ecd2aaf.js"
+  }
+]);
